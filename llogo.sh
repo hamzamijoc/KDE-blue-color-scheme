a93eacd638012b4aa9506529137d28870359ec55
_sp1d3r-ba <sp1d3r-ba@users.noreply.github.com>
2017-10-03 09:46:06 +0200

    Add files via upload

---
+++ llogo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "\n"
+cat /home/sp1d3r/.debianasciilogo
+
+linuxlogo  -g -a -f -F "\\n#EWelcome to             [ #H ]\\n#EOS name                [ #O ]\\n#EVersion of OS [ #V ]\\n#ECPU vendor             [ #X ]\\n#EType of CPU            [    #T ]\\n#ENumber of CPU's        [  #N ]\\n#ERam memory             [   #R ]\\n#U"
